6d9dc3e7f49918196c30c52d995321363cd6251f
Adriano Monteiro Marques <py.adriano@gmail.com>
2007-07-20 01:58:58 +0000

    Added some debugs to watch an issue on windows
    git-svn-id: http://svn.umitproject.org/svnroot/umit/trunk@1069 1105ee14-b0fa-0310-85a5-ff3eed429ff7

--- umitGUI/NmapOutputViewer.py
+++ umitGUI/NmapOutputViewer.py
@@ -349,9 +349,11 @@ gtk.color_selection_palette_to_string([gtk.gdk.Color(*highlight_color),]))
         self.refresh_output()
     
     def refresh_output(self, widget=None):
-        #log.debug("Refresh nmap output")
+        log.debug("Refresh nmap output")
         nmap_of = open(self.nmap_output_file)
-        self.text_buffer.set_text(nmap_of.read())
+        content = nmap_of.read()
+        log.debug("Nmap output to refresh: \n\n%s\n\n" % content)
+        self.text_buffer.set_text(content)
 
         # Closing file to avoid file descriptor problems
         nmap_of.close()
