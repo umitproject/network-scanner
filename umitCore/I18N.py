8e6c481ad2fa754c9960a67e4ed89509da38c2eb
Adriano Monteiro Marques <py.adriano@gmail.com>
2007-07-27 11:42:32 +0000

    Trying something...
    git-svn-id: http://svn.umitproject.org/svnroot/umit/trunk@1134 1105ee14-b0fa-0310-85a5-ff3eed429ff7

--- umitCore/I18N.py
+++ umitCore/I18N.py
@@ -56,7 +56,7 @@ def enc(string):
     """
     log.debug(">>> Converting '%s' from '%s' to unicode" % (string, ENC))
     import pdb; pdb.set_trace()
-    string = string.decode("utf8", ERRORS).encode("utf8", ERRORS)
+    string = string.decode().encode("utf8", ERRORS)
     log.debug(">>> Converted to: '%s'" % string)
 
     return string
