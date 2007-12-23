988b23d92b0d284a35655bdf73e99b1be07b67da
Luis A. Bastao Silva <luis@umitproject.org>
2007-12-23 13:58:28 +0000

    fixed https://sourceforge.net/tracker/?func=detail&atid=752647&aid=1846955&group_id=142490
    TODO: Add rest of dependences and link to UNIX and Mac install scripts
    
    git-svn-id: http://svn.umitproject.org/svnroot/umit/trunk@2825 1105ee14-b0fa-0310-85a5-ff3eed429ff7

---
+++ install_scripts/unix/deps_verify.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+
+# https://sourceforge.net/tracker/?func=detail&atid=752647&aid=1846955&group_id=142490
+echo
+echo "####################################"
+echo "# Umit Dependencies - Verify    "
+echo "####################################"
+echo
+
+#Func check 
+Check(){
+
+if [ -z "$1" ] ; then 
+  exit 0 
+fi 
+
+if [ $? = 0 ]; then
+  echo "## $1  -- checked "
+else 
+   echo "## $1  -- unchecked " 
+fi 
+}
+
+# Python Checking
+python --version 1>/dev/null 2>&1
+Check "Python"
+
+# PyGTK Checking 
+python -c "import gtk"
+Check "PyGTK"
+
+
+
