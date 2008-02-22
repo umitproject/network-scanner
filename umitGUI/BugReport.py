b1b62c50708690659f7c74e7cace254e9ee07122
Guilherme Polo <ggpolo@gmail.com>
2008-02-22 11:50:49 +0000

    - removed CrashReport module; - created a CrashReport class at BugReport's module; - BugReport is a HIGDialog now; - BugReport adapted to serve as CrashReport; - BugRegister sets bugs to not-private by default now; - umit startup script saves crash report to a temporary    file using tempfile.mkstemp if some unexpected error    occurs while starting/running CrashReport dialog.
    git-svn-id: http://svn.umitproject.org/svnroot/umit/trunk@2849 1105ee14-b0fa-0310-85a5-ff3eed429ff7

--- umitGUI/BugReport.py
+++ umitGUI/BugReport.py
@@ -23,26 +23,33 @@ import gtk
 import gobject
 import webbrowser
 
-from higwidgets.higdialogs import HIGAlertDialog
-from higwidgets.higlabels import HIGSectionLabel, HIGHintSectionLabel
+from higwidgets.higdialogs import HIGDialog, HIGAlertDialog
+from higwidgets.higlabels import HIGHintSectionLabel
 from higwidgets.higtables import HIGTable
-from higwidgets.higboxes import HIGHBox, HIGVBox
+from higwidgets.higboxes import HIGHBox
 
 from umitCore.BugRegister import BugRegister
 from umitCore.I18N import _
 
-class BugReport(gtk.Window, object):
-    def __init__(self):
-        gtk.Window.__init__(self)
-        self.set_title(_('Bug Report'))
+class BugReport(HIGDialog):
+    def __init__(self, title=_('Bug Report'), summary=None, description=None,
+                 category=None, crashreport=False):
+        HIGDialog.__init__(self, title=title, 
+            buttons=(gtk.STOCK_OK, gtk.RESPONSE_ACCEPT,
+                gtk.STOCK_CANCEL, gtk.RESPONSE_CANCEL))
+        
         self.set_position(gtk.WIN_POS_CENTER_ALWAYS)
         
-        self.response_id = False
+        self.crashreport = crashreport
         
         self._create_widgets()
         self._set_category_list()
         self._pack_widgets()
         self._connect_widgets()
+        
+        self.summary = summary or ''
+        self.description = description or ''
+        self.category = category or ''
 
     def _set_category_list(self):
         # Obtained at bug tracker page source code
@@ -59,150 +66,172 @@ class BugReport(gtk.Window, object):
         self.category_list.append(["XML Parser", "862567"])
         
     def _create_widgets(self):
-        self.vbox = HIGVBox()
-        self.button_box = gtk.HButtonBox()
-        
-        self.private_check = gtk.CheckButton(_("Only Umit project members should read this \
-bug report"))
-        
         self.category_label = HIGHintSectionLabel(_("Category (optional)"),
-                                                  _("If you know in which section of the \
-program is the bug, please, seclect it from the choosebox. If you don't know what section to \
-choose, leave it blank."))
+            _("If you know in which section of the program "
+            "is the bug, please, seclect it from the choosebox. "
+            "If you don't know what section to choose, leave it blank."))
         self.category_list = gtk.ListStore(str, str)
         self.category_combo = gtk.ComboBoxEntry(self.category_list, 0)
 
         self.email_label = HIGHintSectionLabel(_("Email"),
-                                               _("Please, inform a valid e-mail address, from \
-where you can be reached to be notified when the bug get fixed. Not used for other purposes."))
+            _("Please inform a valid e-mail address from "
+            "where you can be reached to be notified when the bug gets "
+            "fixed. Not used for other purposes."))
         self.email_entry = gtk.Entry()
 
         self.summary_label = HIGHintSectionLabel(_("Summary"),
-                                                 _("This should be a quick description of \
-the issue. Try to be clear and concise."))
+            _("This should be a quick description of the issue. "
+            "Try to be clear and concise."))
         self.summary_entry = gtk.Entry()
 
-        self.description_label = HIGHintSectionLabel(_("Detailed Description"),
-                                                     _("This is where you should really write \
-about the bug, describing it as clear as possible, and giving as many informations as you can \
-along with your system informations, like: Which operating system you're using? \
-Which Nmap version you have insalled?"))
+        self.description_label = HIGHintSectionLabel(_("Description"),
+            _("This is where you should write about the bug, "
+            "describing it as clear as possible and giving as many "
+            "informations as you can along with your system informations, "
+            "like: Which operating system are you using? Which Nmap "
+            "version do you have installed?"))
         self.description_scrolled = gtk.ScrolledWindow()
         self.description_text = gtk.TextView()
 
         self.bug_icon = gtk.Image()
-        self.bug_text = gtk.Label(_("This Bug Report dialog, allows you to easily tell us \
-about a problem that you may have found on Umit. Doing so, you help us to help you, by \
-fixing and improving Umit faster than usual."))
-
-        self.btn_ok = gtk.Button(stock=gtk.STOCK_OK)
-        self.btn_cancel = gtk.Button(stock=gtk.STOCK_CANCEL)
+        self.bug_text = gtk.Label(_("This Bug Report dialog allows you "
+            "to easily tell us about a problem that you may have found on "
+            "Umit. Doing so, you help us to help you, by fixing and "
+            "improving Umit faster than usual."))
 
         self.hbox = HIGHBox()
         self.table = HIGTable()
 
     def _pack_widgets(self):
         self.description_scrolled.add(self.description_text)
-        self.description_scrolled.set_policy(gtk.POLICY_AUTOMATIC, gtk.POLICY_AUTOMATIC)
+        self.description_scrolled.set_policy(gtk.POLICY_AUTOMATIC, 
+            gtk.POLICY_AUTOMATIC)
         self.description_scrolled.set_size_request(400, 150)
         self.description_text.set_wrap_mode(gtk.WRAP_WORD)
 
-        self.bug_icon.set_from_stock(gtk.STOCK_DIALOG_INFO, gtk.ICON_SIZE_DIALOG)
+        self.bug_icon.set_from_stock(gtk.STOCK_DIALOG_INFO, 
+            gtk.ICON_SIZE_DIALOG)
         self.bug_icon.set_padding(10, 0)
         self.bug_text.set_line_wrap(True)
 
         self.hbox.set_border_width(12)
-        self.vbox.set_border_width(6)
         
-        self.table.attach_label(self.category_label, 0, 1, 0, 1)
-        self.table.attach_entry(self.category_combo, 1, 2, 0, 1)
+        nextpos = (0, 1)
+        if not self.crashreport:
+            self.table.attach_label(self.category_label, 0, 1, 0, 1)
+            self.table.attach_entry(self.category_combo, 1, 2, 0, 1)
+            nextpos = (1, 2)
 
-        self.table.attach_label(self.email_label, 0, 1, 1, 2)
-        self.table.attach_entry(self.email_entry, 1, 2, 1, 2)
+        self.table.attach_label(self.email_label, 0, 1, *nextpos)
+        self.table.attach_entry(self.email_entry, 1, 2, *nextpos)
 
-        self.table.attach_label(self.summary_label, 0, 1, 2, 3)
-        self.table.attach_entry(self.summary_entry, 1, 2, 2, 3)
+        nextpos = (2, 3)
+        if not self.crashreport:
+            self.table.attach_label(self.summary_label, 0, 1, 2, 3)
+            self.table.attach_entry(self.summary_entry, 1, 2, 2, 3)
+            nextpos = (3, 4)
 
-        self.table.attach_label(self.description_label, 0, 2, 3, 4)
-        self.table.attach_entry(self.description_scrolled, 0, 2, 4, 5)
-
-        #self.table.attach_label(self.private_check, 0, 2, 5, 6)
+        self.table.attach_label(self.description_label, 0, 2, *nextpos)
+        nextpos = nextpos[0] + 1, nextpos[1] + 1
+        self.table.attach_entry(self.description_scrolled, 0, 2, *nextpos)
 
         self.hbox._pack_noexpand_nofill(self.bug_icon)
         self.hbox._pack_expand_fill(self.bug_text)
 
-        self.button_box.set_layout(gtk.BUTTONBOX_END)
-        self.button_box.pack_start(self.btn_ok)
-        self.button_box.pack_start(self.btn_cancel)
-        
-        self.vbox._pack_noexpand_nofill(self.hbox)
-        self.vbox._pack_expand_fill(self.table)
-        self.vbox._pack_noexpand_nofill(self.button_box)
-        self.add(self.vbox)
+        self.vbox.pack_start(self.hbox, False, False)
+        self.vbox.pack_start(self.table)
 
     def _connect_widgets(self):
-        self.btn_ok.connect("clicked", self.send_report)
-        self.btn_cancel.connect("clicked", self.close)
-        self.connect("delete-event", self.close)
+        self.connect('response', self.check_response)
 
-    def send_report(self, widget):
+    def check_response(self, widget, response_id):
+        if response_id == gtk.RESPONSE_ACCEPT: # clicked on Ok btn
+            self.send_report()
+        elif response_id in (gtk.RESPONSE_DELETE_EVENT, gtk.RESPONSE_CANCEL):
+            # there are tree possibilities to being here:
+            # 1) user clicked on 'x' button
+            # 2) user clicked on 'cancel' button
+            # 3) report was sent successfully and now we can destroy this
+            self.destroy()
+
+    def send_report(self):
+        """Prepare dialog to send a bug report and then call _send_report."""
+        # set cursor to busy cursor (supposing it will take some time
+        # to submit the report)
+        self.window.set_cursor(gtk.gdk.Cursor(gtk.gdk.WATCH))
+
+        # disable dialog controls
+        for child in self.vbox.get_children():
+            child.set_sensitive(False)
+
+        # now send report
+        gobject.idle_add(self._send_report)
+        
+    def restore_state(self):
+        """Restore dialog state, just like it was before calling 
+        send_report."""
+        self.window.set_cursor(None)
+        for child in self.vbox.get_children():
+            child.set_sensitive(True)
+
+    def _send_report(self):
         if self.summary == "" or self.description == "" or self.email == "":
             cancel_dialog = HIGAlertDialog(type=gtk.MESSAGE_ERROR,
-                                               message_format=_("Bug report is incomplete!"),
-                                               secondary_text=_("The bug report is incomplete. \
-You must inform a short summary that describes the issue, a detailed description that \
-explains clearly what is happening and a valid e-mail, so you can be contacted when the bug \
-get fixed."))
+                message_format=_("Bug report is incomplete!"),
+                secondary_text=_("The bug report is incomplete. "
+                    "You must inform a description that explains clearly "
+                    "what is happening and a valid e-mail, so you can be "
+                    "contacted when the bug gets fixed."))
             cancel_dialog.run()
             cancel_dialog.destroy()
-            return None
+            return self.restore_state()
 
         bug_register = BugRegister()
 
-        bug_register.is_private = self.private
         bug_register.category_id = self.category_id
         bug_register.summary = self.summary
-        bug_register.details = "%s\n\nEmail: %s" % (self.description, self.email)
+        bug_register.details = "%s\n\nEmail: %s" % (self.description, 
+            self.email)
         
         bug_page = None
         try:
             bug_page = bug_register.report()
         except:
             cancel_dialog = HIGAlertDialog(type=gtk.MESSAGE_ERROR,
-                                           message_format=_("Bug not reported!"),
-                                           secondary_text=_("The bug description could \
-not be reported. This problem may be caused by the lack of Internet Access or \
-indisponibility of the bug tracker server. Please, verify your internet access, and \
-then try to report the bug once again."))
+                message_format=_("Bug not reported!"),
+                secondary_text=_("The bug description could not be "
+                    "reported. This problem may be caused by the lack "
+                    "of Internet access or indisponibility of the bug "
+                    "tracker server. Please, verify your internet access, "
+                    "and then try to report the bug once again."))
             cancel_dialog.run()
             cancel_dialog.destroy()
+            return self.restore_state()
         else:
             ok_dialog = HIGAlertDialog(type=gtk.MESSAGE_INFO,
-                                       message_format=_("Bug sucessfully reported!"),
-                                       secondary_text=_("The bug description was \
-sucessfully reported. A web page with detailed description about this report is \
-going to be openned in your default web browser."))
+                message_format=_("Bug sucessfully reported!"),
+                secondary_text=_("The bug description was sucessfully "
+                    "reported. A web page with detailed description about "
+                    "this report will be opened in your default web browser "
+                    "now."))
             ok_dialog.run()
             ok_dialog.destroy()
 
-            self.close()
+        if bug_page:
+            try:
+                webbrowser.open(bug_page)
+            except: # XXX What exceptions should be caught here ?
+                page_dialog = HIGAlertDialog(type=gtk.MESSAGE_ERROR,
+                    message_format=_("Could not open default Web Browser"),
+                    secondary_text=_("Umit was unable to open your default "
+                        "web browser to show the bug tracker page with the "
+                        "report status. Try visiting Umit's bug tracker "
+                        "page to see if your bug was reported."))
+                page_dialog.run()
+                page_dialog.destroy()
 
-        try:
-            webbrowser.open(bug_page)
-        except:
-            page_dialog = HIGAlertDialog(type=gtk.MESSAGE_ERROR,
-                                       message_format=_("Could not open default Web Browser"),
-                                       secondary_text=_("Umit was unable to open your default \
-web browser to show the bug tracker page with the report status. Try to visit the Umit's \
-bug tracker page to see if your bug was reported (you won't see if you marked the check box \
-that makes the report private)."))
-            page_dialog.run()
-            page_dialog.destroy()
-
-            self.close()
-
-    def close(self, widget=None, event=None):
-        self.destroy()
+        # report sent successfully
+        self.response(gtk.RESPONSE_DELETE_EVENT)
 
     def get_category(self):
         return self.category_combo.child.get_text()
@@ -223,14 +252,6 @@ that makes the report private)."))
     def set_description(self, description):
         self.description_text.get_buffer().set_text(description)
 
-    def get_private(self):
-        if self.private_check.get_active():
-            return "1"
-        return "0"
-
-    def set_private(self, private):
-        self.private_check.set_active(private)
-
     def get_category_id(self):
         for i in self.category_list:
             if i[0] == self.category:
@@ -243,21 +264,24 @@ that makes the report private)."))
     def set_email(self, email):
         self.email_entry.set_text(email)
 
-    def run_unblocked(self):
-        if not self.modal:
-            self.set_modal(True)
-        self.show_all()
 
     category_id = property(get_category_id)
     category = property(get_category, set_category)
     summary = property(get_summary, set_summary)
     description = property(get_description, set_description)
-    private = property(get_private, set_private)
     email = property(get_email, set_email)
 
-if __name__ == "__main__":
-    w = BugReport()
-    w.show_all()
-    w.connect("delete-event", lambda x, y: gtk.main_quit())
+class CrashReport(BugReport):
+    def __init__(self, summary, description, title=_('Crash Report')):
+        BugReport.__init__(self, title, summary, description, "862568", True)
     
-    gtk.main()
+if __name__ == "__main__":
+    c = BugReport()
+    c.show_all()
+    while True:
+        result = c.run()
+        if result in (gtk.RESPONSE_CANCEL, gtk.RESPONSE_DELETE_EVENT, 
+            gtk.RESPONSE_NONE):
+            c.destroy()
+            break
+    
