9ded5f1806f22c802e705d7ed7100c0307569683
Luis A. Bastao Silva <luis@umitproject.org>
2008-07-20 19:12:29 +0000

    Update licence - FSF address
    Command used: 
    find . -path '*/.svn' -prune -o  -type f -print0 | xargs -0 sed -i -e 's/59 Temple Place, Suite 330/51 Franklin Street, Fifth Floor/;s/02111-1307/02110-1301/;' 
    
    Check licences: licensecheck -c -r trunk_directory/  
    
    git-svn-id: http://svn.umitproject.org/svnroot/umit/trunk@3177 1105ee14-b0fa-0310-85a5-ff3eed429ff7

--- install_scripts/utils/version_update.py
+++ install_scripts/utils/version_update.py
@@ -18,7 +18,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
 # This program updates the Umit version number in all the places it needs to be
 # updated. It takes a single command-line argument, which is the new version
