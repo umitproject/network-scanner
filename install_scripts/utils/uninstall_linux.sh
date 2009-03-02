d00908a67fd172bec94b2f68f69b34e8bfc26837
Guilherme Polo <ggpolo@gmail.com>
2009-03-02 18:59:02 +0000

    Removed unused shell scripts.
    git-svn-id: http://svn.umitproject.org/svnroot/umit/trunk@4246 1105ee14-b0fa-0310-85a5-ff3eed429ff7

--- install_scripts/utils/uninstall_linux.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-rm -rf ~/.umit
-rm -rf /usr/lib/python2.4/site-packages/umitCore
-rm -rf /usr/lib/python2.4/site-packages/umitGUI
-rm -rf /usr/lib/python2.4/site-packages/higwidgets
-rm -rf /usr/share/pixmaps/umit
-rm -rf /usr/share/locale/pt_BR/LC_MESSAGES/umit.mo
-rm -rf /usr/bin/umit
