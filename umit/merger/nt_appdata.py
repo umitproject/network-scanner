adf898b753ea00c8f232133c36e976d98aa34d13
Guilherme Polo <ggpolo@gmail.com>
2009-04-05 05:11:29 +0000

    Added custom exception for managing the extra merge needed. Rename the old directory so this merge doesn't try to run again.
    git-svn-id: http://svn.umitproject.org/svnroot/umit/trunk@4438 1105ee14-b0fa-0310-85a5-ff3eed429ff7

--- umit/merger/nt_appdata.py
+++ umit/merger/nt_appdata.py
@@ -1,7 +1,16 @@
 import os
+import errno
 import shutil
 
 from umit.core import BasePaths
+
+class AlreadyExistsError(Exception):
+    def __init__(self, msg, backup_path):
+        self._msg = msg
+        self.backup_path = backup_path
+
+    def __str__(self):
+        return self._msg
 
 def merge():
     """Moves the old ~/.umit to the new configuration directory, this new
@@ -13,14 +22,19 @@ def merge():
     if BasePaths.UMIT_CFG_DIR == 'umit':
         # This is indeed a version new enough of umit.
         old_path = os.path.join(os.path.expanduser("~"), ".umit")
+        backup_path = old_path + '_backup'
         if os.path.exists(old_path):
+            new_path = os.path.join(BasePaths.HOME, BasePaths.UMIT_CFG_DIR))
             try:
-                shutil.move(old_path,
-                        os.path.join(BasePaths.HOME, BasePaths.UMIT_CFG_DIR))
-            except OSError, err:
-                if err.errno == 17:
-                    # Destination already exists, need to merge the files
-                    # from the old directory to this new one.
-                    # XXX
-                    return
-                raise
+                try:
+                    shutil.move(old_path, new_path)
+                except OSError, err:
+                    if err.errno == errno.EEXIST:
+                        # Destination already exists, need to merge the files
+                        # from the old directory to this new one.
+                        raise AlreadyExistsError("%r" % new_path, backup_path)
+                    raise
+            finally:
+                # Move the old ~/.umit to ~/.umit_backup so next time this
+                # merger runs it will notice the ausence of ~/.umit
+                shutil.move(old_path, backup_path)
