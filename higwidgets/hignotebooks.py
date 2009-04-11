a2e4ca378002c355e967ea02ca4141e9f3a73f23
Francesco Piccinno <stack.box@gmail.com>
2009-04-11 13:54:49 +0000

    Reverting hignotebooks to r3177
    git-svn-id: http://svn.umitproject.org/svnroot/umit/trunk@4493 1105ee14-b0fa-0310-85a5-ff3eed429ff7

--- higwidgets/hignotebooks.py
+++ higwidgets/hignotebooks.py
@@ -6,7 +6,6 @@
 #
 # Author: Adriano Monteiro Marques <adriano@umitproject.org>
 #         Cleber Rodrigues <cleber.gnu@gmail.com>
-#         Francesco Piccinno <stack.box@gmail.com>
 #
 # This library is free software; you can redistribute it and/or modify 
 # it under the terms of the GNU Lesser General Public License as published 
@@ -123,14 +122,6 @@ class HIGEditableLabel(gtk.EventBox):
 gobject.type_register(HIGEditableLabel)
 HIGAnimatedLabel = HIGEditableLabel
 
-gtk.rc_parse_string(""" 
-    style "thinWidget" { 
-        xthickness = 0 
-        ythickness = 0 
-    } 
-    widget "*.tabNotebookButton" style "thinWidget" 
-""") 
- 	
 class HIGNotebook(gtk.Notebook):
     def __init__(self):
         gtk.Notebook.__init__(self)
@@ -152,24 +143,39 @@ class HIGClosableTabLabel(HIGHBox):
     def __create_widgets(self):
         self.label = HIGAnimatedLabel(self.label_text)
         
-        self.image = gtk.image_new_from_stock(gtk.STOCK_CLOSE,
-                                              gtk.ICON_SIZE_MENU)
-        self.button = HIGButton()
-        self.button.set_relief(gtk.RELIEF_NONE)
-        self.button.set_focus_on_click(False)
-        self.button.add(self.image)
-        self.button.set_name('tabNotebookButton')
+        self.close_image = gtk.Image()
+        self.close_image.set_from_stock(gtk.STOCK_CLOSE, gtk.ICON_SIZE_BUTTON)
+        self.close_button = HIGButton()
+        self.close_button.set_size_request(22, 22)
+        self.close_button.set_relief(gtk.RELIEF_NONE)
+        self.close_button.set_focus_on_click(False)
+        self.close_button.add(self.close_image)
 
-        self.button.connect('clicked', self.__on_button_clicked)
-        self.button.connect('style-set', self.__on_button_style_set)
+        self.ok_image = gtk.Image()
+        self.ok_image.set_from_stock(gtk.STOCK_APPLY, gtk.ICON_SIZE_BUTTON)
+        self.ok_button = HIGButton()
+        self.ok_button.set_size_request(22, 22)
+        self.ok_button.set_relief(gtk.RELIEF_NONE)
+        self.ok_button.set_focus_on_click(False)
+        self.ok_button.add(self.ok_image)
+
+        self.close_button.connect('clicked', self.__close_button_clicked)
+        self.ok_button.connect('clicked', self.__ok_button_clicked)
         self.label.connect('button-press-event', self.on_button_press_event)
         self.label.entry.connect('focus-out-event', self.on_entry_focus_out)
-        
-        self.pack_start(self.label, False, False, 0)
-        self.pack_end(self.button, False, False, 0)
+
+        for w in (self.label, self.close_button, self.ok_button):
+            self.pack_start(w, False, False, 0)
 
         self.show_all()
-        self.switch_button_mode(False)
+        self.switch_button_mode(False) # Change to label mode
+
+        # def do_get_property(self, property):
+        #     func = self.property_map.get(property, None)
+        #     if func:
+        #         return func()
+        #     else:
+        #         raise 
 
     def on_entry_focus_out(self, widget, event):
         self.switch_button_mode(False)
@@ -182,26 +188,18 @@ class HIGClosableTabLabel(HIGHBox):
         """Switch button from editing mode (True) to label mode (False)
         """
         if mode:
-            self.image.set_from_stock(gtk.STOCK_APPLY, gtk.ICON_SIZE_MENU)
+            self.close_button.hide()
+            self.ok_button.show()
         else:
-            self.image.set_from_stock(gtk.STOCK_CLOSE, gtk.ICON_SIZE_MENU)
+            self.ok_button.hide()
+            self.close_button.show()
 
-        parent = self.get_parent()
+    def __close_button_clicked(self, widget):
+        self.emit('close-clicked')
 
-        if parent:
-            parent.queue_draw()
-
-    def __on_button_clicked(self, widget):
-        if mode:
-            self.label.on_entry_activated(self.label.entry)
-            self.switch_button_mode(False)
-        else:
-            self.emit('close-clicked')
-
-    def __on_button_style_set(self, widget, prev_style):
-        w, h = gtk.icon_size_lookup_for_settings(widget.get_settings(),
-                                                 gtk.ICON_SIZE_MENU)
-        widget.set_size_request(w + 2, h + 2)
+    def __ok_button_clicked(self, widget):
+        self.label.on_entry_activated(self.label.entry)
+        self.switch_button_mode(False)
 
     def get_text(self):
         return self.label.get_text()
